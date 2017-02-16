03541fdd1cc3ba1c7333aa0e1a57850cc0d85c94
cpnguyen <cpnguyen5@dons.usfca.edu>
2017-02-15 20:46:45 -0800

    Create src dir & DB connection file

---
+++ src/db.py
@@ -0,0 +1,34 @@
+import psycopg2
+from psycopg2 import extras
+
+
+def cursor_connect(cursor_factory=None):
+    """
+    Connects to the DB and returns the connection and cursor, ready to use.
+    Parameters
+    ----------
+    cursor_factory : psycopg2.extras
+    Returns
+    -------
+    (psycopg2.extensions.connection, psycopg2.extensions.cursor)
+        A tuple of (psycopg2 connection, psycopg2 cursor).
+    """
+    #DB connection
+    conn = psycopg2.connect(dbname="mimic", user="mimic", host="localhost", port="2345",
+                            password="oNuemmLeix9Yex7W")
+    if not cursor_factory:
+        cur = conn.cursor()
+    else:
+        cur = conn.cursor(cursor_factory=cursor_factory)
+    return conn, cur
+
+
+if __name__ == "__main__":
+    query = """SELECT subject_id, hadm_id FROM admissions LIMIT 10;"""
+    # conn, cur = cursor_connect()
+    conn, cur = cursor_connect(psycopg2.extras.DictCursor)  # access retrieved records as Python dict using keys
+    cur.execute(query)
+    rows = cur.fetchall()
+    # print rows[0]['subject_id']
+    for row in rows:
+        print row
